7ddc58f0bc7c73530100019baa3367034b4803a3
conceptcatalystcc <conceptcatalystcc@gmail.com>
2023-08-24 01:51:19 +0530

    admin debug

--- app.js
+++ app.js
@@ -53,8 +53,6 @@ const verifyHost = require("./middlewares/verifyHost");
 
 var app = express();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "25mb" }));
 app.use(cors());
 
 const DEFAULT_ADMIN = {
@@ -241,7 +239,8 @@ const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
   }
 );
 app.use(admin.options.rootPath, adminRouter);
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
